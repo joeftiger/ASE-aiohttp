05d7a409920373a6e006550cfd88f71fbe6ec13b
Julius Oeftiger <>
2021-10-09 16:44:25 +0200

    delete unused files

--- tag.py
+++
@@ -1,6 +0,0 @@
-def route_tags() -> str:
-    return "/tags/"
-
-
-def route_tag(tag_id: int) -> str:
-    return route_tags() + str(tag_id)
--- todo.py
+++
@@ -1,6 +0,0 @@
-def route_todos() -> str:
-    return "/todos/"
-
-
-def route_todo(todo_id: int) -> str:
-    return route_todos() + str(todo_id)
